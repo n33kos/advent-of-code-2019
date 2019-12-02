f67207031878e2c38c623901935304a524e74484
Nicholas Suski <n33kos@gmail.com>
2019-12-01 20:00:35 -0700

    Day 1 solution

--- src/fuelRequirements.js
+++ src/fuelRequirements.js
@@ -1,3 +1,33 @@
-export default () => {
-    return true;
+export const fuelForModule = (mass) => {
+  return Math.max(Math.floor(mass / 3) - 2, 0);
+}
+  
+export const fuelForModuleRecursive = (mass) => {
+  let total = 0;
+  let fuelForMass = mass;
+  
+  while (fuelForMass > 0) {
+    fuelForMass = fuelForModule(fuelForMass);
+    total += fuelForMass
+  }
+
+  return total;
+}
+
+export const fuelRequirementsSimple = (modules) => {
+  return modules.reduce(
+    (acc, curr) => {
+
+      return acc + fuelForModule(curr)
+    }, 0
+  );
+}
+
+export const fuelRequirements = (modules) => {
+  return modules.reduce(
+    (acc, curr) => {
+
+      return acc + fuelForModuleRecursive(curr)
+    }, 0
+  );
 }
